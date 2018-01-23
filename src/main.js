26c739184b4f239a213bf8cdb690c891c33c0b78
zhouyuantest <1114802403@qq.com>
2018-01-23 15:59:11 +0800

    vue one pule dome

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import  ezathon from 'ezathon'
+
+
+Vue.use(ezathon)
+
+Vue.config.productionTip = false
+
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
